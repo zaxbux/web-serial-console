5111b7363493e5f71602b79613598a15bf93c494
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-30 13:03:27 -0700

    chore: update xterm.js with new settings

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -99,6 +99,11 @@ export default defineComponent({
 		},
 		setTerminalOptions() {
 			this.$options.platform.terminal.setOption('theme', Settings.themes.default);
+			this.$options.platform.terminal.setOption('cursorStyle', Settings.cursorStyle);
+			this.$options.platform.terminal.setOption('bellStyle', Settings.bellStyle);
+			this.$options.platform.terminal.setOption('cursorBlink', Settings.cursorBlink);
+
+			this.xterm.focus();
 		},
 		async toggleConsole(connected: boolean): Promise<void> {
 			const port = SerialManager.getPort(Settings.portIndex);
