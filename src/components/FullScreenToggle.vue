f4e3ac92387f1f92ca1b1bf92b4674f7c0b3168a
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-29 23:41:25 -0700

    chore: use correct iton

--- src/components/FullScreenToggle.vue
+++ src/components/FullScreenToggle.vue
@@ -1,7 +1,7 @@
 <template>
 	<button class="btn" title="fullscreen" @click="$emit('fullscreen')">
-		<font-awesome-icon v-if="fullscreen" :icon="['far', 'expand-wide']" fixed-width />
-		<font-awesome-icon v-else :icon="['far', 'compress-wide']" fixed-width />
+		<font-awesome-icon v-if="fullscreen" :icon="['far', 'compress-wide']" fixed-width />
+		<font-awesome-icon v-else :icon="['far', 'expand-wide']" fixed-width />
 	</button>
 </template>
 
