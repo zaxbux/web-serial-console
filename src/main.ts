7da0b4b955a66eb14d5b6edbff7d8077cc998d44
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-30 00:54:47 -0700

    chore: register worker

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { FontAwesomeIcon } from './plugins/fontawesome';
 import settings from './plugins/settings';
 import './utils/dark-mode';
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+});
+
 createApp(App)
 	.use(settings)
 	.component('font-awesome-icon', FontAwesomeIcon)
