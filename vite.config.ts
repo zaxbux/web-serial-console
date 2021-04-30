8ebf699ffefb2361dc324e799fb4bd12c0d59046
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-29 23:42:17 -0700

    chore: add service worker boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [
+		vue(),
+		VitePWA({
+			srcDir: 'src',
+			filename: 'sw.ts',
+			base: '/',
+			strategies: 'injectManifest',
+			manifest: {
+				"name": "Serial Console",
+				"short_name": "Console",
+				"description": "A web-based serial console.",
+				"lang": "en-CA",
+				"start_url": "/?source=pwa",
+				"display": "standalone",
+				"theme_color": "#212121",
+				"icons": [
+					{
+						"src": "assets/icon.svg",
+						"sizes": "any"
+					},
+					{
+						"src": "assets/icon.png",
+						"sizes": "144x144",
+						"type": "image/png"
+					}
+				]
+			}
+		}),
+	],
+});
