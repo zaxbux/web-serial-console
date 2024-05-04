45c2ad6e831e6728ea9e4fe0b09b86ddb427de98
Zachary Schneider <hello@zacharyschneider.ca>
2024-05-04 00:52:51 -0700

    fix: unsupported modal

--- src/components/SerialApiUnsupportedModal.vue
+++ src/components/SerialApiUnsupportedModal.vue
@@ -1,8 +1,13 @@
 <template>
-  <v-dialog :modal-value="true">
+  <v-dialog persistent :model-value="true">
     <v-card title="Error!">
       <v-card-text>
-        This browser does not support the Web Serial API.
+        <p>This browser does not support the Web Serial API.</p>
+
+        <p>Want support in your browser?</p>
+        <ul>
+          <li><a href="https://connect.mozilla.org/t5/discussions/fully-support-web-usb-and-web-serial/m-p/62">Firefox</a></li>
+        </ul>
       </v-card-text>
 
       <v-card-actions>
