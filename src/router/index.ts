08d035abe5f29b8f021f653d8eed1706debac5be
Zach <hello@zacharyschneider.ca>
2025-04-26 16:59:39 -0700

    chore: update deps

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,17 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+//import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { routes } from 'vue-router/auto-routes'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: setupLayouts,
+  //extendRoutes: setupLayouts,
+  routes: setupLayouts(routes),
 })
 
 export default router
