ec658b32cfd47e770f70abe4a508e24fe9004620
Zach <hello@zacharyschneider.ca>
2025-04-26 17:00:09 -0700

    chore: custom button toggle

---
+++ src/components/BtnToggle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const modelValue = defineModel()
+
+defineProps<{
+  label?: string
+  hint?: string
+  items: { value: any, title: string }[]
+}>()
+</script>
+<template>
+	<div>
+    <div v-if="label" class="text-subtitle-2">{{ label }}</div>
+    <v-btn-toggle v-model="modelValue" mandatory divided density="compact" variant="outlined">
+      <v-btn v-for="item, key in items" :key="key" :value="item.value" :text="item.title" size="small" slim/>
+    </v-btn-toggle>
+    <div v-if="hint" class="text-caption text-medium-emphasis">{{ hint }}</div>
+  </div>
+</template>
